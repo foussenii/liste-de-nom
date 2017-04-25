308e729efb87088aab30b5e27d27c6a20ab910fe
fousseni <foussenicoulibalypro@gmail.com>
2017-04-25 14:40:48 +0200

    ajout img

---
+++ list.js
@@ -0,0 +1,18 @@
+(function() {
+
+              var bodyEl = $('body'),
+                  navToggleBtn = bodyEl.find('.nav-toggle-btn');
+
+              navToggleBtn.on('click', function(e) {
+                  bodyEl.toggleClass('active-nav');
+                  e.preventDefault();
+              });
+
+          })();
+          $(function() {
+
+                    setTimeout(function() {
+                        $('.fly-in-text').removeClass('hidden');
+                    }, 500);
+
+                })();
